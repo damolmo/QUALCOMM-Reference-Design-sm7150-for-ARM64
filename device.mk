6ac9e4b0c80ae7d21c64a8a4a8d29e5071cd4610
David <davidmolinamorales1999@gmail.com>
2021-08-31 15:35:57 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-Boot control HAL
+#Boot control HAL
 #PRODUCT_PACKAGES += \
     #android.hardware.boot@1.0-impl \
     #android.hardware.boot@1.0-service
