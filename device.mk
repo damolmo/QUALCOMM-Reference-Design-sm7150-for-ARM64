abf9555973328066f895cf8a047e0816eee020ea
David <davidmolinamorales1999@gmail.com>
2021-08-31 15:33:42 +0200

    we're removing this for now

--- device.mk
+++ device.mk
@@ -30,23 +30,23 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+Boot control HAL
+#PRODUCT_PACKAGES += \
+    #android.hardware.boot@1.0-impl \
+    #android.hardware.boot@1.0-service
 
-PRODUCT_PACKAGES += \
-    bootctrl.sm6150
+#PRODUCT_PACKAGES += \
+    #bootctrl.sm6150
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.sm6150 \
-    libgptutils \
-    libz \
-    libcutils
+#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    #bootctrl.sm6150 \
+    #libgptutils \
+    #libz \
+    #libcutils
 
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
+#PRODUCT_PACKAGES += \
+    #otapreopt_script \
+    #cppreopts.sh \
+    #update_engine \
+    #update_verifier \
+    #update_engine_sideload
